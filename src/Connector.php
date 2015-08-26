60e9aa327ed88528b8dd4f1ea2e94872f4b52cd9
Pierre Groth <pierre@elbcoast.net>
2015-08-27 01:18:11 +0200

    Added methods for shards and zone events

--- src/Connector.php
+++ src/Connector.php
@@ -2,10 +2,124 @@
 
 namespace Ecn\RiftConnector;
 
+use GuzzleHttp\ClientInterface;
+
 class Connector
 {
-    public function __construct()
+    // Servers
+    const EU_SERVER = "http://chat-eu.riftgame.com:8080";
+    const US_SERVER = "http://chat-us.riftgame.com:8080";
+
+    // Endpoints
+    const ENDPOINT_SHARD_LIST = "/chatservice/shard/list";
+    const ENDPOINT_ZONEEVENT_LIST = "/chatservice/zoneevent/list?shardId=%s";
+
+    /**
+     * @var string Url of the chat server
+     */
+    protected $server;
+
+    /**
+     * @var array List of shards
+     */
+    protected $shardList;
+
+    /**
+     * @var Client The Guzzle HTTP Client
+     */
+    protected $client;
+
+
+    /**
+     * Constructor.
+     *
+     * @param ClientInterface $client
+     * @param string          $server
+     */
+    public function __construct(ClientInterface $client, $server = self::EU_SERVER)
     {
+        $this->client = $client;
+        $this->server = $server;
+    }
+
+
+    /**
+     * Returns a list of shards on the current chat server
+     *
+     * @return array
+     */
+    public function getShardList()
+    {
+        if (!$this->shardList) {
+            $this->shardList = $this->retrieveShardList();
+        }
+
+        return $this->shardList;
+    }
+
+
+    /**
+     * Returns a list of zone events for a shard
+     *
+     * @param $shardId
+     *
+     * @return mixed
+     *
+     * @throws Exception\ConnectionException
+     */
+    public function getZoneEvents($shardId)
+    {
+
+        $response = $this->client->get(sprintf($this->server.self::ENDPOINT_ZONEEVENT_LIST, $shardId));
+
+        if ($response->getStatusCode() == "200") {
+            $json =  $response->json();
+
+            return $json['data'];
+        }
+
+        throw new Exception\ConnectionException("Unable to retrieve zone events");
+    }
+
+
+    /**
+     * Retrieves the shard list from the server, hydrates and caches it
+     *
+     * @return array
+     *
+     * @throws Exception\ConnectionException
+     */
+    protected function retrieveShardList()
+    {
+        $response = $this->client->get($this->server.self::ENDPOINT_SHARD_LIST);
+
+        if ($response->getStatusCode() == "200") {
+           $json =  $response->json();
+
+            return $this->hydrateShardList($json['data']);
+        }
+
+        throw new Exception\ConnectionException("Unable to retrieve shard list");
+    }
+
+
+    /**
+     * Hydrates the shard list into a more useful format
+     *
+     * @param $shardList
+     *
+     * @return array
+     */
+    protected function hydrateShardList($shardList)
+    {
+
+        $hydratedShardList = array();
+
+        foreach ($shardList as $shard) {
+            $hydratedShardList[$shard['shardId']] = $shard;
+        }
+
+        return $hydratedShardList;
     }
 }
 
--- tests/ConnectorTest.php
+++ tests/ConnectorTest.php
@@ -9,7 +9,137 @@ class ConnectorTest extends \PHPUnit_Framework_TestCase
 
     public function testIfcanInitClass()
     {
-        $connector = new Connector();
+        $client = $this->getMock('\GuzzleHttp\ClientInterface');
+        $connector = new Connector($client);
+    }
+
+
+    public function testRetrieveShardList()
+    {
+        // Assume a successful response
+        $response = $this->getMock('\GuzzleHttp\Message\ResponseInterface');
+        $response->method('getStatusCode')->willReturn('200');
+        $response->method('json')->willReturn($this->getDummyShardList());
+
+        $client = $this->getMock('\GuzzleHttp\ClientInterface');
+        $client->method('get')->willReturn($response);
+
+        $connector = new Connector($client);
+
+        $shardList = $connector->getShardList();
+        $this->assertNotEmpty($shardList);
+
+        $shard = array_pop($shardList);
+
+        $this->assertArrayHasKey('shardId', $shard);
+    }
+
+
+    /**
+     *  @expectedException \Ecn\RiftConnector\Exception\ConnectionException
+     */
+    public function testRetrieveShardListError()
+    {
+        $response = $this->getMock('\GuzzleHttp\Message\ResponseInterface');
+        $response->method('getStatusCode')->willReturn('404');
+
+        $client = $this->getMock('\GuzzleHttp\ClientInterface');
+        $client->method('get')->willReturn($response);
+
+        $connector = new Connector($client);
+        $connector->getShardList();
+    }
+
+
+    public function testRetrieveZoneEvents()
+    {
+        // Assume a successful response
+        $response = $this->getMock('\GuzzleHttp\Message\ResponseInterface');
+        $response->method('getStatusCode')->willReturn('200');
+        $response->method('json')->willReturn($this->getDummyZoneEvents());
+
+        $client = $this->getMock('\GuzzleHttp\ClientInterface');
+        $client->method('get')->willReturn($response);
+
+        $connector = new Connector($client);
+        $zoneEvents = $connector->getZoneEvents('2741');
+
+        $this->assertNotEmpty($zoneEvents);
+    }
+
+
+    /**
+     *  @expectedException \Ecn\RiftConnector\Exception\ConnectionException
+     */
+    public function testRetrieveZoneEventsError()
+    {
+        $response = $this->getMock('\GuzzleHttp\Message\ResponseInterface');
+        $response->method('getStatusCode')->willReturn('404');
+
+        $client = $this->getMock('\GuzzleHttp\ClientInterface');
+        $client->method('get')->willReturn($response);
+
+        $connector = new Connector($client);
+        $connector->getZoneEvents('2741');
+    }
+
+
+    /**
+     * Some dummy values
+     *
+     * @return mixed
+     */
+    private function getDummyShardList()
+    {
+        $data = '{
+        "status":"success",
+        "data":[
+        {"shardId":2501,"name":"Blightweald","running":false,"ready":false,"locked":false,"popLevel":0,"queueWaitTime":0,"pvp":false,"language":1},
+        {"shardId":2502,"name":"Cloudborne","running":false,"ready":false,"locked":false,"popLevel":0,"queueWaitTime":0,"pvp":true,"language":1},
+        {"shardId":2504,"name":"Sagespire","running":false,"ready":false,"locked":false,"popLevel":0,"queueWaitTime":0,"pvp":true,"language":1},
+        {"shardId":2507,"name":"Bloodiron","running":false,"ready":false,"locked":false,"popLevel":0,"queueWaitTime":0,"pvp":true,"language":1},
+        {"shardId":2722,"name":"Zaviel","running":false,"ready":false,"locked":false,"popLevel":0,"queueWaitTime":0,"pvp":false,"language":1},
+        {"shardId":2741,"name":"Typhiria","running":false,"ready":false,"locked":false,"popLevel":0,"queueWaitTime":0,"pvp":false,"language":1}
+        ]}';
+
+        return json_decode($data, true);
+    }
+
+    private function getDummyZoneEvents()
+    {
+        $data = '{
+        "status":"success",
+        "data":[
+        {"zone":"Kingdom of Pelladane","zoneId":479431687},
+        {"zone":"Cape Jule","zoneId":1770829751},
+        {"zone":"Shimmersand","zoneId":6},
+        {"zone":"Stillmoor","zoneId":26,"name":"Witch of the West","started":1440628412},
+        {"zone":"Freemarch","zoneId":19},
+        {"zone":"Silverwood","zoneId":12},
+        {"zone":"Ember Isle","zoneId":1992854106},
+        {"zone":"Iron Pine Peak","zoneId":22},
+        {"zone":"Scarlet Gorge","zoneId":26580443},
+        {"zone":"Gloamwood","zoneId":27},
+        {"zone":"Stonefield","zoneId":1481781477},
+        {"zone":"Droughtlands","zoneId":336995470},
+        {"zone":"Scarwood Reach","zoneId":20,"name":"Infernal Awakening","started":1440629309},
+        {"zone":"The Dendrome","zoneId":282584906},
+        {"zone":"Moonshade Highlands","zoneId":24},
+        {"zone":"Ashora","zoneId":790513416},
+        {"zone":"Steppes of Infinity","zoneId":798793247},
+        {"zone":"Seratos","zoneId":1494372221},
+        {"zone":"City Core","zoneId":1967477725,"name":"Unstable City Core","started":1440629019},
+        {"zone":"Eastern Holdings","zoneId":1213399942},
+        {"zone":"Ardent Domain","zoneId":1446819710},
+        {"zone":"Kingsward","zoneId":1300766935},
+        {"zone":"Morban","zoneId":956914599},
+        {"zone":"Goboro Reef","zoneId":301},
+        {"zone":"Draumheim","zoneId":302,"name":"Howling Fury","started":1440628818},
+        {"zone":"Tarken Glacier","zoneId":303},
+        {"zone":"Tyrant\u0027s Throne","zoneId":426135797}
+        ]}';
+
+        return json_decode($data, true);
     }
 
 }
