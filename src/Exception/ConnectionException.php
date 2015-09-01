f7ddf5903e2f621630556611f915a643276a98f9
Pierre Groth <pierre@elbcoast.net>
2015-09-01 18:37:02 +0200

    Added docblock to ConnectionException

--- src/Exception/ConnectionException.php
+++ src/Exception/ConnectionException.php
@@ -2,6 +2,16 @@
 
 namespace Ecn\RiftConnector\Exception;
 
+/**
+ * Class ConnectionException
+ *
+ * PHP Version 5.4
+ *
+ * @author    Pierre Groth <pierre@elbcoast.net>
+ * @copyright 2015
+ * @license   MIT
+ *
+ */
 class ConnectionException extends \Exception
 {
 }
